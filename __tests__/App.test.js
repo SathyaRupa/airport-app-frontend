b9c2099b94aa7c4ea1012cff1436f7f7f65d0c1f
Shaz Ahammed <shaz.ahammed@thoughtworks.com>
2024-05-16 15:45:57 +0530

    working test

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,9 +1,22 @@
-import React from "react";
-import {render} from "@testing-library/react-native"
-import App from "../App";
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import App from '../App';
+import Homepage from '../screens/Homepage';
+import AirlinesHome from '../screens/airlines/AirlinesHome';
+
+jest.mock('@react-navigation/native-stack', () => ({
+  createNativeStackNavigator: jest.fn(() => ({
+    Navigator: jest.fn(),
+    Screen: jest.fn(),
+  })),
+}));
+
+jest.mock('@react-navigation/native', () => ({
+  NavigationContainer: ({children}) => <>{children}</>,
+}));
 
 describe('App screen', () => {
   it('should render App', () => {
-    expect(render(<App/>)).toBeTruthy();
-  })
-})
+    expect(render(<App />)).toBeTruthy();
+  });
+});
