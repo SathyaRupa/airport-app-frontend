acd3052d47e4aa25d017a3c1734b0898a1d66f50
Shaz Ahammed <shaz.ahammed@thoughtworks.com>
2024-05-16 16:47:59 +0530

    refactoring

--- __tests__/HomePage.test.js
+++ __tests__/HomePage.test.js
@@ -8,23 +8,20 @@ describe('Home page', () => {
     };
 
     const homePage = render(<Homepage navigation={mockNavigation} />);
-    expect(homePage.queryByText('Airport App')).toBeTruthy();
-
     const aircraftsButton = homePage.queryByText('Aircrafts');
-    expect(aircraftsButton).toBeTruthy();
-    fireEvent.press(aircraftsButton);
-
     const airlinesButton = homePage.queryByText('Airlines');
-    expect(airlinesButton).toBeTruthy();
-    fireEvent.press(airlinesButton);
-    expect(mockNavigation.push).toHaveBeenCalledWith('AirlinesHome');
-
     const gatesButton = homePage.queryByText('Gates');
-    expect(gatesButton).toBeTruthy();
-    fireEvent.press(gatesButton);
-
     const slotsButton = homePage.queryByText('Slots');
+
+    expect(homePage.queryByText('Airport App')).toBeTruthy();
+    
+    expect(aircraftsButton).toBeTruthy();
+    expect(airlinesButton).toBeTruthy();
     expect(slotsButton).toBeTruthy();
-    fireEvent.press(slotsButton);
+    expect(gatesButton).toBeTruthy();
+
+    fireEvent.press(airlinesButton);
+
+    expect(mockNavigation.push).toHaveBeenCalledWith('AirlinesHome');
   });
 });
