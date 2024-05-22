4213470fb15e337370689032a1baaac56eb44be2
Sathya Rupa.S <sathyarupa2002@gmail.com>
2024-05-22 12:45:45 +0530

    Create AirlineDetails.js and add route in App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Homepage from './screens/Homepage';
 import AirlinesHome from './screens/airlines/AirlinesHome';
+import AirlineDetails from './screens/airlines/AirlineDetails';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const stack = createNativeStackNavigator();
@@ -9,9 +10,10 @@ function App() {
   return (
     <>
       <NavigationContainer>
-      <stack.Navigator>
+        <stack.Navigator>
           <stack.Screen name="Home" component={Homepage} />
           <stack.Screen name="Airlines" component={AirlinesHome} />
+          <stack.Screen name="Airline Details" component={AirlineDetails} />
         </stack.Navigator>
       </NavigationContainer>
     </>
---
+++ screens/airlines/AirlineDetails.js
@@ -0,0 +1,8 @@
+import {useEffect} from 'react';
+import airlinesService from '../../helpers/airlinesService';
+
+function AirlineDetails() {
+  useEffect;
+}
+
+export default AirlineDetails;
