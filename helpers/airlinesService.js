3a9124626becce121edd25244696b4d387166882
Shaz Ahammed <153790994+shaz-ahammed@users.noreply.github.com>
2024-05-22 18:03:41 +0530

    [Shaz] URL compatible with both ios and android (#11)

--- helpers/airlinesService.js
+++ helpers/airlinesService.js
@@ -1,23 +1,14 @@
 import axios from 'axios';
+import url from "./url"
 
 export default {
-  fetchAllAirlines: async page => {
-    try {
-      const response = await axios.get(
-        `http://127.0.0.1:8080/airlines?page=${page}`,
-      );
-      return response.data;
-    } catch (error) {
-      console.log('Error fetching data: ', error);
-    }
-  },
-
-  fetchAirlineDetails: async id => {
-    try {
-      const response = await axios.get(`http://127.0.0.1:8080/airline/${id}`);
-      return response.data;
-    } catch (error) {
-      console.log('Error fetching data: ', error);
+    fetchAllAirlines: async (page) => {
+        try{
+        const response = await axios.get(`${url}/airlines?page=${page}`)
+        return response.data
+        } catch (error) {
+            console.log("Error fetching data: ", error)
+        }
     }
   },
 };
