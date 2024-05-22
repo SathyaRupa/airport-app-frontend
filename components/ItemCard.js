e6b1109a78cefac375c9d4fd994a115e460156c5
Sathya Rupa.S <sathyarupa2002@gmail.com>
2024-05-22 12:56:21 +0530

    Make airlines clickable

--- components/ItemCard.js
+++ components/ItemCard.js
@@ -1,29 +1,32 @@
 import * as React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import {Card, Text} from 'react-native-paper';
 
 function ItemCard({name, icon}) {
   return (
     <View style={styles.mainCard}>
-      <Card style={styles.itemCard}>
-        <LinearGradient colors={['#4D869C', '#7AB2B2']}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 1 }}
-        style={{
-          borderRadius: 12,
-          overflow: 'hidden',
-        }}>
-          <Card.Content>
-            <View style={styles.content}>
-              {icon}
-              <Text style={styles.text} variant="titleLarge">
-                {name}
-              </Text>
-            </View>
-          </Card.Content>
-        </LinearGradient>
-      </Card>
+      <TouchableOpacity>
+        <Card style={styles.itemCard}>
+          <LinearGradient
+            colors={['#4D869C', '#7AB2B2']}
+            start={{x: 0, y: 0}}
+            end={{x: 1, y: 1}}
+            style={{
+              borderRadius: 12,
+              overflow: 'hidden',
+            }}>
+            <Card.Content>
+              <View style={styles.content}>
+                {icon}
+                <Text style={styles.text} variant="titleLarge">
+                  {name}
+                </Text>
+              </View>
+            </Card.Content>
+          </LinearGradient>
+        </Card>
+      </TouchableOpacity>
     </View>
   );
 }
