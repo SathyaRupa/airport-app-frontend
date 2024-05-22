49642024b7bee51c8c20f8383f0e19999aa49a20
Shaz Ahammed <153790994+shaz-ahammed@users.noreply.github.com>
2024-05-22 18:03:41 +0530

    [Shaz] URL compatible with both ios and android (#11)
    * url compatible with both ios and android
    * separated protocol from ip
    * removed java from CI steps

--- helpers/airlinesService.js
+++ helpers/airlinesService.js
@@ -1,9 +1,10 @@
 import axios from "axios"
+import url from "./url"
 
 export default {
     fetchAllAirlines: async (page) => {
         try{
-        const response = await axios.get(`http://127.0.0.1:8080/airlines?page=${page}`)
+        const response = await axios.get(`${url}/airlines?page=${page}`)
         return response.data
         } catch (error) {
             console.log("Error fetching data: ", error)
