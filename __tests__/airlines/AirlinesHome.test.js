f7b867f0d56d3f4ce874f9c976da4a2f1df82091
shaz-ahammed <shazahammed2000@gmail.com>
2024-05-28 12:57:01 +0530

    tests for airlines home page for error

--- __tests__/airlines/AirlinesHome.test.js
+++ __tests__/airlines/AirlinesHome.test.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {fireEvent, render, waitFor} from '@testing-library/react-native';
 import AirlinesHome from '../../screens/airlines/AirlinesHome';
 import airlinesService from '../../helpers/airlinesService';
@@ -14,32 +15,64 @@ jest.mock('../../helpers/airlinesService', () => ({
 
 describe('Airlines home - get all airlines', () => {
   let mockNavigation;
+
   beforeEach(() => {
     mockNavigation = {
       push: jest.fn(),
     };
+    jest.clearAllMocks();
   });
+
   it('snapshot test', () => {
     const airlinesHome = render(<AirlinesHome navigation={mockNavigation} />);
 
     expect(airlinesHome).toMatchSnapshot();
   });
-  it('should render an item card', () => {
-    const airlinesHome = render(<AirlinesHome navigation={mockNavigation} />);
-    airlinesService.fetchAll.mockReturnValue([
+
+  it('should render an item card', async () => {
+    useIsFocused.mockReturnValue(true);
+
+    airlinesService.fetchAll.mockResolvedValue([
       {name: 'Jet Airways', count: '6'},
     ]);
+    const airlinesHome = render(<AirlinesHome navigation={mockNavigation} />);
     const itemCard = airlinesHome.queryAllByTestId('item-card');
 
     expect(itemCard).toBeTruthy();
-    waitFor(() => {
+    await waitFor(() => {
       expect(airlinesHome.getByText('Jet Airways')).toBeTruthy();
     });
   });
-  it('should not render an item card when no data is recieved', () => {
+
+  it('should not render an item card when no data is recieved', async () => {
     airlinesService.fetchAll.mockResolvedValue([]);
 
-    const {queryByText} = render(<AirlinesHome navigation={mockNavigation} />);
-    waitFor(() => expect(queryByText('Jet Airways')).toBeNull());
+    const airlinesHome = render(<AirlinesHome navigation={mockNavigation} />);
+    expect(airlinesHome.queryByTestId('item-card')).toBeNull();
+    await waitFor(() =>
+      expect(airlinesHome.queryByText('Jet Airways')).toBeNull(),
+    );
+  });
+
+  it('should navigate to Create Airlines page when clicked on create', () => {
+    const {getByText} = render(<AirlinesHome navigation={mockNavigation} />);
+    const createButton = getByText('Create');
+    expect(createButton).toBeTruthy();
+    fireEvent.press(createButton);
+    expect(mockNavigation.push).toHaveBeenCalledWith('Create Airline');
+  });
+
+  it('should handle error when fetchAll throws an error', async () => {
+    useIsFocused.mockReturnValue(true);
+
+    airlinesService.fetchAll.mockRejectedValue(new Error('Failed to fetch'));
+
+    const {queryByTestId} = render(
+      <AirlinesHome navigation={mockNavigation} />,
+    );
+
+    await waitFor(() => {
+      expect(queryByTestId('item-card')).toBeNull();
+    });
   });
 });
