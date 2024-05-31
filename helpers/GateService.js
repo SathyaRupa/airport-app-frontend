2612f11cbc9e63ce4dc46e5d044a53d47d97cac4
jaicharan <jaicharan.santhanam@thoughtworks.com>
2024-05-31 17:21:47 +0530

    Implemented Filter feature for get all gates

--- helpers/GateService.js
+++ helpers/GateService.js
@@ -2,9 +2,17 @@ import axios from 'axios';
 import url from './Url';
 
 export default {
-  fetchAll: async page => {
+  fetchAll: async (page, floor) => {
     try {
-      const response = await axios.get(`${url}/gates?page=${page}`);
+      console.log(' floor no', floor);
+      // if (floor <= 0) {
+      //   const response = await axios.get(`${url}/gates?page=${page}`);
+      //   return response.data;
+      // } else {
+      const response = await axios.get(
+        `${url}/gates?page=${page}&floor=${floor}`,
+      );
+      console.log(response.data);
       return response.data;
     } catch (error) {
       console.log('Error fetching data: ', error);
--- screens/gates/GatesHome.js
+++ screens/gates/GatesHome.js
@@ -2,9 +2,25 @@ import ItemCard from '../../components/ItemCard';
 import * as React from 'react';
 import {useEffect, useState} from 'react';
 import GateService from '../../helpers/GateService';
-import {FlatList, ActivityIndicator} from 'react-native';
-import {Avatar} from 'react-native-paper';
+import {
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+  View,
+  SafeAreaView,
+  TextInput,
+} from 'react-native';
+import {
+  Avatar,
+  Button,
+  PaperProvider,
+  Text,
+  Portal,
+  Dialog,
+} from 'react-native-paper';
 import {useIsFocused} from '@react-navigation/native';
+import CreateButton from '../../components/CreateButton';
+import FilterButton from '../../components/FilterButton';
 
 function GatesHome({navigation}) {
   const [allDataLoaded, setAllDataLoaded] = useState(false);
@@ -12,15 +28,23 @@ function GatesHome({navigation}) {
   const [gates, setGates] = useState([]);
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(0);
+  const [floor, setFloor] = useState('');
+  const [filterRender, setFilterRender] = useState(false);
+
+  const [visible, setVisible] = React.useState(false);
+  const showDialog = () => setVisible(true);
+  const hideDialog = () => setVisible(false);
+
   const isFocused = useIsFocused();
 
   useEffect(() => {
     async function fetchData() {
       if (!allDataLoaded && isFocused && !fetchedPages.includes(page)) {
-        const response = await GateService.fetchAll(page);
+        const response = await GateService.fetchAll(page, floor);
         setFetchedPages(prevPages => [...prevPages, page]);
         if (response.length === 0) {
           setAllDataLoaded(true);
+          setFilterRender(true);
         } else {
           setGates(prevGates => [...prevGates, ...response]);
         }
@@ -28,13 +52,24 @@ function GatesHome({navigation}) {
       setLoading(false);
     }
     fetchData();
-  }, [page, allDataLoaded, isFocused]);
+  }, [page, allDataLoaded, isFocused, filterRender]);
 
   const loadMoreGates = () => {
     if (!allDataLoaded) {
       setPage(prevPage => prevPage + 1);
     }
   };
+  function handleFilter() {
+    if (isFocused) {
+      setGates([]);
+      setPage(0);
+      setAllDataLoaded(false);
+      setLoading(true);
+      setFetchedPages([]);
+    }
+    setFilterRender(true);
+    hideDialog();
+  }
 
   useEffect(() => {
     if (isFocused) {
@@ -63,22 +98,76 @@ function GatesHome({navigation}) {
   );
 
   return (
-    <FlatList
-      data={gates}
-      renderItem={itemData => (
-        <ItemCard
-          id={itemData.item.id}
-          value={'Gate ' + itemData.item.gate_number}
-          icon={icon}
-          testId={`item-card-${itemData.index}`}
-          onPress={() => handlePress(itemData.item.id)}
+    <PaperProvider>
+      <SafeAreaView style={styles.mainContainer}>
+        <View style={styles.headerContainer}>
+          {/* <View style={styles.filterContainer}>
+          <Text style={{textAlignVertical: 'center'}}>Filter by:</Text>
+          <TextInput
+            onChangeText={newText => setFloor(newText)}
+            placeholder=" Floor No"
+            style={{borderRadius: 10}}></TextInput>
+          <Button style={{paddingTop: 10}} onPress={handleFilter}>
+            Submit
+          </Button>
+        </View> */}
+          <FilterButton handleOnPress={showDialog} />
+          <Portal>
+            <Dialog visible={visible} onDismiss={hideDialog}>
+              <Dialog.Content>
+                <Text variant="bodyMedium">Filter Floor number</Text>
+                <TextInput
+                  onChangeText={newText => setFloor(newText)}
+                  placeholder=" Floor No"
+                  style={{borderRadius: 10}}></TextInput>
+              </Dialog.Content>
+              <Dialog.Actions>
+                <Button onPress={handleFilter}>Done</Button>
+              </Dialog.Actions>
+            </Dialog>
+          </Portal>
+          <CreateButton
+            handleOnPress={() => {
+              navigation.push('Create Airline');
+            }}
+            testId="create-button"
+          />
+        </View>
+
+        <FlatList
+          data={gates}
+          renderItem={itemData => (
+            <ItemCard
+              id={itemData.item.id}
+              value={'Gate ' + itemData.item.gate_number}
+              icon={icon}
+              testId={`item-card-${itemData.index}`}
+              onPress={() => handlePress(itemData.item.id)}
+            />
+          )}
+          onEndReached={loadMoreGates}
+          onEndReachedThreshold={0.5}
+          ListFooterComponent={renderFooter}
         />
-      )}
-      onEndReached={loadMoreGates}
-      onEndReachedThreshold={0.5}
-      ListFooterComponent={renderFooter}
-    />
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
 
 export default GatesHome;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    padding: 10,
+    flex: 1,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  filterContainer: {
+    paddingLeft: 15,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+});
